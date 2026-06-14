8f5a9d28002f57fe2d731f20540279c56dabc3ca
gourab <gourab.pal.gpal@gmail.com>
2026-06-14 21:47:36 +0530

    cred management

--- src/test/java/org/example/db/SupabaseDB.java
+++ src/test/java/org/example/db/SupabaseDB.java
@@ -1,3 +1,31 @@
+//package org.example.db;
+//
+//import io.qameta.allure.Step;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//
+//public class SupabaseDB {
+//
+//    private static Connection connection;
+//    private static final String URL = "jdbc:postgresql://aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres?sslmode=require";
+//    private static final String USER = "postgres.hbfnaltjcpuohnvfwldg";
+//    private static final String PASSWORD = "postgres@4972";
+//
+//    @Step("Establishing Superbase database connection")
+//    public static Connection getConnection() {
+//        try {
+//            if (connection == null || connection.isClosed()) {
+//                Class.forName("org.postgresql.Driver");
+//                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("Superbase DB connection failed", e);
+//        }
+//        return connection;
+//    }
+//}
 package org.example.db;
 
 import io.qameta.allure.Step;
@@ -8,11 +36,23 @@ import java.sql.DriverManager;
 public class SupabaseDB {
 
     private static Connection connection;
-    private static final String URL = "jdbc:postgresql://aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres?sslmode=require";
-    private static final String USER = "postgres.hbfnaltjcpuohnvfwldg";
-    private static final String PASSWORD = "postgres@4972";
 
-    @Step("Establishing Superbase database connection")
+    private static final String URL =
+            System.getenv().getOrDefault(
+                    "SUPABASE_DB_URL",
+                    "jdbc:postgresql://aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres?sslmode=require"
+            );
+
+    private static final String USER =
+            System.getenv().getOrDefault(
+                    "SUPABASE_DB_USER",
+                    "postgres.hbfnaltjcpuohnvfwldg"
+            );
+
+    private static final String PASSWORD =
+            System.getenv("SUPABASE_DB_PASSWORD");
+
+    @Step("Establishing Supabase database connection")
     public static Connection getConnection() {
         try {
             if (connection == null || connection.isClosed()) {
@@ -20,9 +60,9 @@ public class SupabaseDB {
                 connection = DriverManager.getConnection(URL, USER, PASSWORD);
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Superbase DB connection failed", e);
+            throw new RuntimeException("Supabase DB connection failed", e);
         }
+
         return connection;
     }
 }
