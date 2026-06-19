b073f28f6dbf631de6937244658fad03dd7465be
gourab <gourab.pal.gpal@gmail.com>
2026-06-20 01:20:05 +0530

    testing faikure

--- src/test/java/org/example/tests/SauceDemo/InventoryTest.java
+++ src/test/java/org/example/tests/SauceDemo/InventoryTest.java
@@ -20,7 +20,6 @@ public class InventoryTest extends BaseTest {
     @Severity(SeverityLevel.NORMAL)
     public void sidebarLinksShouldVisible() {
         InventoryPage inventoryPage = new LoginPage().loginAs("standard_user", "secret_sauce");
-        Assert.fail("demo test fail for email check");
         inventoryPage.validateAllOptionsInSidenav();
     }
 }
