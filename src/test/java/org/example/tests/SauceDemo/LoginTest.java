dca13d76fe0442242021fdea2f21889c27712251
gourab <gourab.pal.gpal@gmail.com>
2026-06-20 14:45:36 +0530

    dummy

--- src/test/java/org/example/tests/SauceDemo/LoginTest.java
+++ src/test/java/org/example/tests/SauceDemo/LoginTest.java
@@ -8,7 +8,6 @@ import io.qameta.allure.Story;
 import org.example.core.BaseTest;
 import org.example.pages.saucedemo.InventoryPage;
 import org.example.pages.saucedemo.LoginPage;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Epic("Swag Lab Site")
@@ -21,5 +20,6 @@ public class LoginTest extends BaseTest {
     public void shouldLoginWithStandardUser() {
         InventoryPage inventoryPage = new LoginPage().loginAs("standard_user", "secret_sauce");
         inventoryPage.isInventoryPageLoaded();
+        throw new RuntimeException("run time error");
     }
 }
