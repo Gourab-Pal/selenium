b5d2b9f86d73310f9e5897a8a74edd8afd60084d
gourab <gourab.pal.gpal@gmail.com>
2026-06-27 00:17:20 +0530

    db teardown fix

--- src/test/java/org/example/db/SupabaseDB.java
+++ src/test/java/org/example/db/SupabaseDB.java
@@ -7,7 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class SupabaseDB {
-    private static Connection connection;
+    //private static Connection connection;
     private static final String URL = System.getenv("SUPABASE_DB_URL");
     private static final String USER = System.getenv("SUPABASE_DB_USER");
     private static final String PASSWORD = System.getenv("SUPABASE_DB_PASSWORD");
@@ -15,14 +15,12 @@ public class SupabaseDB {
     @Step("Establishing Supabase database connection")
     public static Connection getConnection() {
         try {
-            if (connection == null || connection.isClosed()) {
-                Class.forName(TestConfig.getJdbcDriver());
-                connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                AllureLogger.log("Supabase DB connected");
-            }
+            Class.forName(TestConfig.getJdbcDriver());
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            AllureLogger.log("Supabase DB connected");
+            return connection;
         } catch (Exception e) {
             throw new RuntimeException("Supabase DB connection failed", e);
         }
-        return connection;
     }
 }
